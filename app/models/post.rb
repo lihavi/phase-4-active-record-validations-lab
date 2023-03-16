5c7bfedf18fa6941527222d5f28cd4b7a01bd394
vivianne <viviannjoroge91@gmail.com>
2023-03-16 17:46:27 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :clickbait_title
+
+  private
+
+  def clickbait_title
+    if title.present? && !title.match?(/(Won't Believe|Secret|Top \d+|Guess)/)
+      errors.add(:title, "must be clickbait-y")
+    end
+  end
 end
